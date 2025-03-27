38e0dc3e9f41ad30dbdd7afe4ad5320952b638bf
snovakow <snovakow@gmail.com>
2025-03-26 20:14:07 -0400

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Scott on 2025-03-26.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
